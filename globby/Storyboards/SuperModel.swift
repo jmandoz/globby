1946b440f1b3cca2c1371a1733ae57289ea0740c
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-14 16:29:39 -0600

    Adds gitignore, adds supermodel class

---
+++ globby/Storyboards/SuperModel.swift
@@ -0,0 +1,21 @@
+//
+//  SuperModel.swift
+//  globby
+//
+//  Created by Jason Mandozzi on 6/14/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
+
+class SuperModel {
+    
+    var intelligence = 0
+    var gonzo = "Loud"
+    
+    init(intelligence: Int, gonzo: String) {
+        self.intelligence = intelligence
+        self.gonzo = gonzo
+    }
+}
+
---
+++ globby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  globby
+//
+//  Created by Jason Mandozzi on 6/14/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
